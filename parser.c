6583c0a1c72bcc905b9e5b707adf1ac56c0427c1
matteowong <mwong8@stuy.edu>
2018-03-01 11:36:22 -0500

    done?

--- parser.c
+++ parser.c
@@ -28,7 +28,7 @@ The file follows the following format:
 	 scale: create a scale matrix, 
 	    then multiply the transform matrix by the scale matrix - 
 	    takes 3 arguments (sx, sy, sz)
-	 translate: create a translation matrix, 
+	 move: create a translation matrix, 
 	    then multiply the transform matrix by the translation matrix - 
 	    takes 3 arguments (tx, ty, tz)
 	 rotate: create an rotation matrix,
@@ -65,22 +65,93 @@ void parse_file ( char * filename,
     f = stdin;
   else
     f = fopen(filename, "r");
+
+  struct matrix * temp;
   
   while ( fgets(line, 255, f) != NULL ) {
-    line[strlen(line)-1]='\0';
+    if (line[strlen(line)-1]=='\n') line[strlen(line)-1]='\0';
     printf(":%s:\n",line);
 
 
     if (!strcmp(line,"line")) {
-      fgets(line,255,f);
-      line[strlen(line)-1]='\0';
+      fgets(line,255,f);//gets next line w/ six commands
+      double x0=0;
+      double y0=0;
+      double z0=0;
+      double x1=0;
+      double y1=0;
+      double z1=0;
+      if (line[strlen(line)-1]=='\n') line[strlen(line)-1]='\0';
+      sscanf(line,"%lf %lf %lf %lf %lf %lf",&x0,&y0,&z0,&x1,&y1,&z1);
+      add_edge(edges,x0,y0,z0,x1,y1,z1);
 
     } else if (!strcmp(line,"ident")) {
+      ident(transform);
+    } else if (!strcmp(line,"scale")) {
+      fgets(line,255,f);
+      if (line[strlen(line)-1]=='\n') line[strlen(line)-1]='\0';
+      double sx=0;
+      double sy=0;
+      double sz=0;
+      sscanf(line,"%lf %lf %lf",&sx,&sy,&sz);
+      temp = make_scale(sx,sy,sz);
+      matrix_mult(temp,transform);
+      free_matrix(temp);
 
+    } else if (!strcmp(line,"move")) {
+      fgets(line,255,f);
+      if (line[strlen(line)-1]=='\n') line[strlen(line)-1]='\0';      
+      double tx=0;
+      double ty=0;
+      double tz=0;
+      sscanf(line,"%lf %lf %lf",&tx,&ty,&tz);
+      temp=make_translate(tx,ty,tz);
+      matrix_mult(temp,transform);
+      free_matrix(temp);
+    } else if (!strcmp(line,"rotate")) {
+      fgets(line,255,f);
+      if (line[strlen(line)-1]=='\n') line[strlen(line)-1]='\0';      
+      char axis[2];
+      double theta;
+      sscanf(line,"%s %lf",axis,&theta);
+      if (axis[0]=='x') {
+	temp=make_rotX(theta);
+      }
+      else if (axis[0]=='y') {
+	temp=make_rotY(theta);
+      }
+      else if (axis[0]=='z') {
+	temp=make_rotZ(theta);
+      }
+      matrix_mult(temp,transform);
+      free_matrix(temp);
+    } else if (!strcmp(line,"apply")) {
+      matrix_mult(transform,edges);
+    } else if (!strcmp(line,"display")) {
+      clear_screen(s);
+      color c;
+      c.green=255;
+      c.red=0;
+      c.blue=0;
+      draw_lines(edges,s,c);
+      display(s);
+    } else if (!strcmp(line,"save")) {
+      fgets(line,255,f);
+      if (line[strlen(line)-1]=='\n') line[strlen(line)-1]='\0';      
+      clear_screen(s);
+      color c;
+      c.green=255;
+      c.red=0;
+      c.blue=0;
+      draw_lines(edges,s,c);
+      save_extension(s,line);
+    } else if (!strcmp(line,"quit")) {
+
+      exit(0);
     }
 
-
-    
   }
 }
   
+
+
